d51d2ca6e1dcd4e498011568c41cce87656d412b
VitaliiBobylov <bobylevvitalyanatolievich@gmail.com>
2025-05-31 10:06:45 +0300

    4

--- js/task-1.js
+++ js/task-1.js
@@ -1,41 +1,20 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//   const totalPrice = quantity * pricePerDroid;
-  
-//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
-
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//   const totalPrice = quantity * pricePerDroid;
-
-//   if (totalPrice > customerCredits) {
-//     return "Insufficient funds!";
-//   }
-
-//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// }
-
-
-// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
-
-function slugify(title) {
-  return title.toLowerCase().split(" ").join("-");
+function isEnoughCapacity(products, containerSize) {
+  const totalItems = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
+  return totalItems <= containerSize;
 }
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,49 +1,26 @@
-// function getElementWidth(content, padding, border) {
-//   const contentWidth = parseFloat(content);
-//   const paddingWidth = parseFloat(padding) * 2;
-//   const borderWidth = parseFloat(border) * 2;
 
-//   return contentWidth + paddingWidth + borderWidth;
-// }
+const profile = {
+    username: "Jacob",
+  playTime: 300,
 
-
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-// function checkForSpam(message) {
-//   const lowerMessage = message.toLowerCase();
-//   return lowerMessage.includes("spam") || lowerMessage.includes("sale");
-// }
-
-
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-
-
-function filterArray(numbers, value) {
-  if (!Array.isArray(numbers)) return [];
-  const result = [];
-  
-  for (let i = 0; i < numbers.length; i++) {
-    if (typeof numbers[i] === "number" && numbers[i] > value) {
-      result.push(numbers[i]);
-    }
-  }
-  
-  return result;
+  changeUsername(newName){
+this.username = newName;
+  },
+  updatePlayTime(hours){
+    this.playTime = hours;
+  },
+getInfo(){
+    return `${this.newName} has ${this.playTime} active hours!`;
+}
 }
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
